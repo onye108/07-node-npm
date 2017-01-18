6e3c7b2be4cd422705b55a0d7e7ad954372d9d2a
Lord Chika Onyebueke <onye108@icloud.com>
2017-01-17 17:40:00 -0800

    complete

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,20 +1,30 @@
 'use strict';
 
-// TODO: Initialize your project using NPM to create and populate a package.json file
-
-// TODO: Require the Express package that you installed via NPM, and instantiate the app
+// DONE: Initialize your project using NPM to create and populate a package.json file
+const express = require('express')
+const app = express();
+// DONE: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
 
 const PORT = process.env.PORT || 3000;
 
-// TODO: Include all of the static resources as an argument to app.use()
+app.use(express.static('./public'));
+// DONE: Include all of the static resources as an argument to app.use()
 
-app.get('*', function(request, response) {
-  // TODO: Using the response object, send the index.html file back to the user
+app.get('/index.html', function(request, response) {
+  response.sendFile('vendor/index.html', { root: './public'});
 });
+// DONE: Using the response object, send the index.html file back to the user
 
-// TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+app.get('/new.html', function(request, response) {
+  response.sendFile('vendor/new.html', { root: './public'});
+});
+// DONE: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+app.get('*', function(request, response) {
+  response.send('ERROR');
+})
 
 app.listen(PORT, function() {
-  // TODO: Log to the console a message that lets you know which port your server has started on
+  console.log(`Server is now listening on Port ${PORT}`);
+  // DONE: Log to the console a message that lets you know which port your server has started on
 });
